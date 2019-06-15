6ecf340aee10787c1122ffb5d267619d379853e2
Eugene G <you@example.com>
2019-06-15 13:59:26 -0700

    features is in helpers

--- helpers.py
+++ helpers.py
@@ -6,8 +6,12 @@ import torch
 from six import iteritems
 from subprocess import Popen, PIPE
 from datetime import datetime
+import cv2
+from PIL import Image
+import numpy as np
 
 ###################################################################
+## Train Helpers
 
 def save_model(ARGS, type, model_dir, model, log_file_path, epoch):
     if epoch % ARGS.model_save_interval == 0 or epoch == ARGS.epochs:
@@ -54,3 +58,57 @@ def print_and_log(log_file_path, string_to_write):
         log_file.write(t + string_to_write + "\n")
 
 ###################################################################
+## Features Helpers
+
+def l2_norm(input, axis = 1):
+    norm = torch.norm(input, 2, axis, True)
+    output = torch.div(input, norm)
+
+    return output
+
+def extract_features(image_data_rgb, model, device, tta = True):
+
+    image_BGR = cv2.cvtColor(image_data_rgb, cv2.COLOR_RGB2BGR)
+
+    # resize image to [128, 128]
+    resized = cv2.resize(image_BGR, (128, 128)) # (160, 160, 3) -> (128, 128, 3)
+    
+    # cv2.imwrite(name,resized)
+    
+    # center crop image
+    a=int((128-112)/2) # x start
+    b=int((128-112)/2+112) # x end
+    c=int((128-112)/2) # y start
+    d=int((128-112)/2+112) # y end
+    ccropped = resized[a:b, c:d] # center crop the image
+    ccropped = ccropped[...,::-1] # BGR to RGB
+
+    # flip image horizontally
+    flipped = cv2.flip(ccropped, 1)
+
+    # load numpy to tensor
+    ccropped = ccropped.swapaxes(1, 2).swapaxes(0, 1)
+    ccropped = np.reshape(ccropped, [1, 3, 112, 112])
+    ccropped = np.array(ccropped, dtype = np.float32)
+    ccropped = (ccropped - 127.5) / 128.0
+    ccropped = torch.from_numpy(ccropped)
+
+    flipped = flipped.swapaxes(1, 2).swapaxes(0, 1)
+    flipped = np.reshape(flipped, [1, 3, 112, 112])
+    flipped = np.array(flipped, dtype = np.float32)
+    flipped = (flipped - 127.5) / 128.0
+    flipped = torch.from_numpy(flipped)
+
+    # extract features
+    model.eval() # set to evaluation mode
+    with torch.no_grad():
+        if tta:
+            emb_batch = model(ccropped.to(device)).cpu() + model(flipped.to(device)).cpu()
+            features = l2_norm(emb_batch)
+        else:
+            features = l2_norm(model(ccropped.to(device)).cpu())
+            
+#     np.save("features.npy", features) 
+#     features = np.load("features.npy")
+
+    return features
