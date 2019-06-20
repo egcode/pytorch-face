bcc7832105d422164e394b912af17fffac39e628
Eugene G <you@example.com>
2019-06-20 00:39:36 -0700

    generalized distance to helpers

--- demo_compare_images.py
+++ demo_compare_images.py
@@ -7,27 +7,24 @@ from __future__ import print_function
 #################################################################################
 #################################################################################
 #################################################################################
-ARCFACE LOSS (doesn't work for now)
+ARCFACE LOSS - face.evoLVe.PyTorch (doesn't work for now)
 #################################################################################
 
-##### NOT SAME        without extract_feature distance = [0.5334694] 
-#                     with extract_feature distance = []
+##### NOT SAME        without extract_feature distance = [1.043365702033043] 
 python3 demo_compare_images.py \
 --model ./pth/backbone_ir50_ms1m_epoch120.pth \
 --image_one_path ./data/golovan_160/Liuba/IMG_0179.png \
 --image_two_path ./data/golovan_160/Julia/0001.png \
 --distance_metric 1
 
-##### NOT SAME        without extract_feature distance = [0.49472308]
-#                     with extract_feature distance = []
+##### NOT SAME        without extract_feature distance = [1.0594086982309818]
 python3 demo_compare_images.py \
 --model ./pth/backbone_ir50_ms1m_epoch120.pth \
 --image_one_path ./data/golovan_160/Alex/haweF.png \
 --image_two_path ./data/golovan_160/Julia/0001.png \
 --distance_metric 1
 
-##### NOT SAME        without extract_feature distance = [0.456529]
-#                     with extract_feature distance = []
+##### NOT SAME        without extract_feature distance = [0.9408659227192402]
 python3 demo_compare_images.py \
 --model ./pth/backbone_ir50_ms1m_epoch120.pth \
 --image_one_path ./data/golovan_160/Eugene/hawfd.png \
@@ -35,29 +32,112 @@ python3 demo_compare_images.py \
 --distance_metric 1
 
 
+##### NOT SAME        without extract_feature distance = [0.8103613704442978]
+python3 demo_compare_images.py \
+--model ./pth/backbone_ir50_ms1m_epoch120.pth \
+--image_one_path ./data/golovan_160/Eugene/IMG_0083.png \
+--image_two_path ./data/golovan_160/Alex/hawefds.png \
+--distance_metric 1
 
-##### SAME            without extract_feature distance = [0.3180063]
-#                     with extract_feature distance = []
+
+
+
+
+##### SAME            without extract_feature distance = [0.4351288080215454]
 python3 demo_compare_images.py \
 --model ./pth/backbone_ir50_ms1m_epoch120.pth \
 --image_one_path ./data/golovan_160/Julia/0003.png \
 --image_two_path ./data/golovan_160/Julia/0001.png \
 --distance_metric 1
 
-##### SAME            without extract_feature distance = [0.50955397]
-#                     with extract_feature distance = []
+##### SAME            without extract_feature distance = [0.9727675095200539]
 python3 demo_compare_images.py \
 --model ./pth/backbone_ir50_ms1m_epoch120.pth \
 --image_one_path ./data/golovan_160/Julia/0003.png \
 --image_two_path ./data/golovan_160/Julia/0004.png \
 --distance_metric 1
 
-##### SAME 
-
+##### SAME            without extract_feature distance = [0.7395100891590118]
 python3 demo_compare_images.py \
 --model ./pth/backbone_ir50_ms1m_epoch120.pth \
+--image_one_path ./data/golovan_160/Eugene/IMG_9489.png \
+--image_two_path ./data/golovan_160/Eugene/IMG_E7066.png \
+--distance_metric 1
+
+##### SAME            without extract_feature distance = [0.37082868814468384]
+python3 demo_compare_images.py \
+--model ./pth/backbone_ir50_ms1m_epoch120.pth \
+--image_one_path ./data/golovan_160/Alex/harfds.png \
+--image_two_path ./data/golovan_160/Alex/hawefd.png \
+--distance_metric 1
+
+
+#################################################################################
+#################################################################################
+#################################################################################
+ARCFACE LOSS-Eugene Casia
+#################################################################################
+
+
+##### NOT SAME        without extract_feature distance = [1.0465557985007763] 
+python3 demo_compare_images.py \
+--model ./pth/IR_50_MODEL_arcface_casia_epoch21.pth \
+--image_one_path ./data/golovan_160/Liuba/IMG_0179.png \
+--image_two_path ./data/golovan_160/Julia/0001.png \
+--distance_metric 1
+
+##### NOT SAME        without extract_feature distance = [0.708492249250412]
+python3 demo_compare_images.py \
+--model ./pth/IR_50_MODEL_arcface_casia_epoch21.pth \
+--image_one_path ./data/golovan_160/Alex/haweF.png \
+--image_two_path ./data/golovan_160/Julia/0001.png \
+--distance_metric 1
+
+##### NOT SAME        without extract_feature distance = [0.8642878979444504]
+python3 demo_compare_images.py \
+--model ./pth/IR_50_MODEL_arcface_casia_epoch21.pth \
+--image_one_path ./data/golovan_160/Eugene/hawfd.png \
+--image_two_path ./data/golovan_160/Julia/IMG_1922.png \
+--distance_metric 1
+
+
+##### NOT SAME        without extract_feature distance = [0.8804192990064621]
+python3 demo_compare_images.py \
+--model ./pth/IR_50_MODEL_arcface_casia_epoch21.pth \
+--image_one_path ./data/golovan_160/Eugene/IMG_0083.png \
+--image_two_path ./data/golovan_160/Alex/hawefds.png \
+--distance_metric 1
+
+
+
+
+
+##### SAME            without extract_feature distance = [0.1647181510925293]
+python3 demo_compare_images.py \
+--model ./pth/IR_50_MODEL_arcface_casia_epoch21.pth \
+--image_one_path ./data/golovan_160/Julia/0003.png \
+--image_two_path ./data/golovan_160/Julia/0001.png \
+--distance_metric 1
+
+##### SAME            without extract_feature distance = [0.27410292625427246]
+python3 demo_compare_images.py \
+--model ./pth/IR_50_MODEL_arcface_casia_epoch21.pth \
 --image_one_path ./data/golovan_160/Julia/0003.png \
 --image_two_path ./data/golovan_160/Julia/0004.png \
+--distance_metric 1
+
+##### SAME            without extract_feature distance = [0.163324773311615]
+python3 demo_compare_images.py \
+--model ./pth/IR_50_MODEL_arcface_casia_epoch21.pth \
+--image_one_path ./data/golovan_160/Eugene/IMG_9489.png \
+--image_two_path ./data/golovan_160/Eugene/IMG_E7066.png \
+--distance_metric 1
+
+##### SAME            without extract_feature distance = [0.0513153076171875]
+python3 demo_compare_images.py \
+--model ./pth/IR_50_MODEL_arcface_casia_epoch21.pth \
+--image_one_path ./data/golovan_160/Alex/harfds.png \
+--image_two_path ./data/golovan_160/Alex/hawefd.png \
 --distance_metric 1
 
 
@@ -67,56 +147,71 @@ python3 demo_compare_images.py \
 #################################################################################
 CENTER LOSS
 #################################################################################
-##### NOT SAME       without extract_feature distance = [0.2699489]
-#                     with extract_feature distance = [1.1721498]
+
+
+
+##### NOT SAME        without extract_feature distance = [1.1721499] 
 python3 demo_compare_images.py \
 --model ./pth/IR_50_MODEL_centerloss_casia_epoch16.pth \
 --image_one_path ./data/golovan_160/Liuba/IMG_0179.png \
 --image_two_path ./data/golovan_160/Julia/0001.png \
 --distance_metric 0
 
-##### NOT SAME       without extract_feature distance = [0.21732879]
-#                     with extract_feature distance = [1.2014594]
+##### NOT SAME        without extract_feature distance = [1.2014594]
 python3 demo_compare_images.py \
 --model ./pth/IR_50_MODEL_centerloss_casia_epoch16.pth \
 --image_one_path ./data/golovan_160/Alex/haweF.png \
 --image_two_path ./data/golovan_160/Julia/0001.png \
 --distance_metric 0
 
-##### NOT SAME       without extract_feature distance = [0.27035767]
-#                     with extract_feature distance = [1.0704521]
+##### NOT SAME        without extract_feature distance = [1.2041703]
 python3 demo_compare_images.py \
 --model ./pth/IR_50_MODEL_centerloss_casia_epoch16.pth \
---image_one_path ./data/golovan_160/Alex/haweF.png \
---image_two_path ./data/golovan_160/Eugene/IMG_E7066.png \
+--image_one_path ./data/golovan_160/Eugene/hawfd.png \
+--image_two_path ./data/golovan_160/Julia/IMG_1922.png \
+--distance_metric 0
+
+
+##### NOT SAME        without extract_feature distance = [0.86273426]
+python3 demo_compare_images.py \
+--model ./pth/IR_50_MODEL_centerloss_casia_epoch16.pth \
+--image_one_path ./data/golovan_160/Eugene/IMG_0083.png \
+--image_two_path ./data/golovan_160/Alex/hawefds.png \
 --distance_metric 0
 
 
 
-##### SAME            without extract_feature distance = [0.12518325]
-#                     with extract_feature distance = [0.58154273]
+
+
+##### SAME            without extract_feature distance = [0.58154285]
 python3 demo_compare_images.py \
 --model ./pth/IR_50_MODEL_centerloss_casia_epoch16.pth \
 --image_one_path ./data/golovan_160/Julia/0003.png \
 --image_two_path ./data/golovan_160/Julia/0001.png \
 --distance_metric 0
 
-##### SAME            without extract_feature distance = [0.21716458]
-#                     with extract_feature distance = [0.8795128]
+##### SAME            without extract_feature distance = [0.8795128]
 python3 demo_compare_images.py \
 --model ./pth/IR_50_MODEL_centerloss_casia_epoch16.pth \
 --image_one_path ./data/golovan_160/Julia/0003.png \
 --image_two_path ./data/golovan_160/Julia/0004.png \
 --distance_metric 0
 
-
-##### SAME            without extract_feature distance = [0.16182399]
-#                     with extract_feature distance = [0.5000807]
+##### SAME            without extract_feature distance = [0.50008076]
 python3 demo_compare_images.py \
 --model ./pth/IR_50_MODEL_centerloss_casia_epoch16.pth \
 --image_one_path ./data/golovan_160/Eugene/IMG_9489.png \
 --image_two_path ./data/golovan_160/Eugene/IMG_E7066.png \
 --distance_metric 0
+
+##### SAME            without extract_feature distance = [0.25166833]
+python3 demo_compare_images.py \
+--model ./pth/IR_50_MODEL_centerloss_casia_epoch16.pth \
+--image_one_path ./data/golovan_160/Alex/harfds.png \
+--image_two_path ./data/golovan_160/Alex/hawefd.png \
+--distance_metric 0
+
+
 
 
 '''
@@ -185,23 +280,6 @@ def main(ARGS):
     print(dist)
     print("=================")
     
-
-def distance(embeddings1, embeddings2, distance_metric=0):
-    if distance_metric==0:
-        # Euclidian distance
-        diff = np.subtract(embeddings1, embeddings2)
-        dist = np.sum(np.square(diff),1)
-    elif distance_metric==1:
-        # Distance based on cosine similarity
-        dot = np.sum(np.multiply(embeddings1, embeddings2), axis=1)
-        norm = np.linalg.norm(embeddings1, axis=1) * np.linalg.norm(embeddings2, axis=1)
-        similarity = dot / norm
-        dist = np.arccos(similarity) / math.pi
-    else:
-        raise 'Undefined distance metric %d' % distance_metric 
-        
-    return dist
-
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
