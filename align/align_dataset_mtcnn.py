161420af0207a9bd832f3b7c7d85d3f6de4979a0
Eugene G <you@example.com>
2019-07-05 11:06:24 -0700

    align images logging fixed

--- align/align_dataset_mtcnn.py
+++ align/align_dataset_mtcnn.py
@@ -5,9 +5,9 @@ from __future__ import print_function
 """Performs face alignment and stores face thumbnails in the output directory."""
 '''
 for N in {1..4}; do \
-python3 align_dataset_mtcnn.py \
-../data/lfw \
-../data/lfw_112 \
+python3 align/align_dataset_mtcnn.py \
+./data/lfw \
+./data/lfw_112 \
 --image_size 112 \
 --margin 32 \
 --random_order \
@@ -16,14 +16,27 @@ python3 align_dataset_mtcnn.py \
 
 
 for N in {1..4}; do \
-python3 align_dataset_mtcnn.py \
-../data/calfw \
-../data/calfw_112 \
+python3 align/align_dataset_mtcnn.py \
+./data/calfw \
+./data/calfw_112 \
 --image_size 112 \
 --margin 32 \
 --random_order \
 --gpu_memory_fraction 0.25 \
 & done
+
+
+for N in {1..2}; do \
+python3 align/align_dataset_mtcnn.py \
+./data/cplfw \
+./data/cplfw_112 \
+--image_size 112 \
+--margin 32 \
+--random_order \
+--gpu_memory_fraction 0.25 \
+& done
+
+
 '''
 # MIT License
 # 
@@ -53,25 +66,57 @@ import os
 import argparse
 import tensorflow as tf
 import numpy as np
-import facenet
 import align.detect_face
 import random
 from time import sleep
 
-def main(args):
+class ImageClass():
+    "Stores the paths to images for a given class"
+    def __init__(self, name, image_paths):
+        self.name = name
+        self.image_paths = image_paths
+  
+    def __str__(self):
+        return self.name + ', ' + str(len(self.image_paths)) + ' images'
+  
+    def __len__(self):
+        return len(self.image_paths)
+
+def get_image_paths(facedir):
+    image_paths = []
+    if os.path.isdir(facedir):
+        images = os.listdir(facedir)
+        image_paths = [os.path.join(facedir,img) for img in images]
+    return image_paths
+
+def get_dataset(path, has_class_directories=True):
+    dataset = []
+    path_exp = os.path.expanduser(path)
+    classes = [path for path in os.listdir(path_exp) \
+                    if os.path.isdir(os.path.join(path_exp, path))]
+    classes.sort()
+    nrof_classes = len(classes)
+    for i in range(nrof_classes):
+        class_name = classes[i]
+        facedir = os.path.join(path_exp, class_name)
+        image_paths = get_image_paths(facedir)
+        dataset.append(ImageClass(class_name, image_paths))
+  
+    return dataset
+
+def main(ARGS):
     sleep(random.random())
-    output_dir = os.path.expanduser(args.output_dir)
+    output_dir = os.path.expanduser(ARGS.output_dir)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     # Store some git revision info in a text file in the log directory
     src_path,_ = os.path.split(os.path.realpath(__file__))
-    facenet.store_revision_info(src_path, output_dir, ' '.join(sys.argv))
-    dataset = facenet.get_dataset(args.input_dir)
+    dataset = get_dataset(ARGS.input_dir)
     
     print('Creating networks and loading parameters')
     
     with tf.Graph().as_default():
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=ARGS.gpu_memory_fraction)
         sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
         with sess.as_default():
             pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
@@ -82,23 +127,27 @@ def main(args):
 
     # Add a random key to the filename to allow alignment using multiple processes
     random_key = np.random.randint(0, high=99999)
-    bounding_boxes_filename = os.path.join(output_dir, 'bounding_boxes_%05d.txt' % random_key)
+    bounding_boxes_filename = os.path.join(output_dir, ARGS.prefix + 'bounding_boxes_%05d.txt' % random_key)
     
     with open(bounding_boxes_filename, "w") as text_file:
         nrof_images_total = 0
         nrof_successfully_aligned = 0
-        if args.random_order:
+        if ARGS.random_order:
             random.shuffle(dataset)
         for cls in dataset:
             output_class_dir = os.path.join(output_dir, cls.name)
+            output_class_dir_failed = os.path.join(output_dir, ARGS.prefix + 'FAILED', cls.name)
             if not os.path.exists(output_class_dir):
                 os.makedirs(output_class_dir)
-                if args.random_order:
+                if ARGS.random_order:
                     random.shuffle(cls.image_paths)
+            if not os.path.exists(output_class_dir_failed):
+                os.makedirs(output_class_dir_failed)
             for image_path in cls.image_paths:
                 nrof_images_total += 1
                 filename = os.path.splitext(os.path.split(image_path)[1])[0]
                 output_filename = os.path.join(output_class_dir, filename+'.jpg')
+                output_filename_failed = os.path.join(output_class_dir_failed, filename+'.jpg')
                 print(image_path)
                 if not os.path.exists(output_filename):
                     try:
@@ -109,7 +158,7 @@ def main(args):
                     else:
                         if img.ndim<2:
                             print('Unable to align "%s"' % image_path)
-                            text_file.write('%s\n' % (output_filename))
+                            text_file.write('Failed  %s\n' % (output_filename))
                             continue
                         if img.ndim == 2:
                             img = facenet.to_rgb(img)
@@ -122,7 +171,7 @@ def main(args):
                             det_arr = []
                             img_size = np.asarray(img.shape)[0:2]
                             if nrof_faces>1:
-                                if args.detect_multiple_faces:
+                                if ARGS.detect_multiple_faces:
                                     for i in range(nrof_faces):
                                         det_arr.append(np.squeeze(det[i]))
                                 else:
@@ -138,15 +187,15 @@ def main(args):
                             for i, det in enumerate(det_arr):
                                 det = np.squeeze(det)
                                 bb = np.zeros(4, dtype=np.int32)
-                                bb[0] = np.maximum(det[0]-args.margin/2, 0)
-                                bb[1] = np.maximum(det[1]-args.margin/2, 0)
-                                bb[2] = np.minimum(det[2]+args.margin/2, img_size[1])
-                                bb[3] = np.minimum(det[3]+args.margin/2, img_size[0])
+                                bb[0] = np.maximum(det[0]-ARGS.margin/2, 0)
+                                bb[1] = np.maximum(det[1]-ARGS.margin/2, 0)
+                                bb[2] = np.minimum(det[2]+ARGS.margin/2, img_size[1])
+                                bb[3] = np.minimum(det[3]+ARGS.margin/2, img_size[0])
                                 cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
-                                scaled = misc.imresize(cropped, (args.image_size, args.image_size), interp='bilinear')
+                                scaled = misc.imresize(cropped, (ARGS.image_size, ARGS.image_size), interp='bilinear')
                                 nrof_successfully_aligned += 1
                                 filename_base, file_extension = os.path.splitext(output_filename)
-                                if args.detect_multiple_faces:
+                                if ARGS.detect_multiple_faces:
                                     output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
                                 else:
                                     output_filename_n = "{}{}".format(filename_base, file_extension)
@@ -154,27 +203,24 @@ def main(args):
                                 text_file.write('%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
                         else:
                             print('Unable to align "%s"' % image_path)
-                            text_file.write('%s\n' % (output_filename))
-                            
+                            text_file.write('Failed  %s\n' % (output_filename))
+                            misc.imsave(output_filename_failed, img)
+
     print('Total number of images: %d' % nrof_images_total)
     print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
             
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
-    
     parser.add_argument('input_dir', type=str, help='Directory with unaligned images.')
     parser.add_argument('output_dir', type=str, help='Directory with aligned face thumbnails.')
-    parser.add_argument('--image_size', type=int,
-        help='Image size (height, width) in pixels.', default=182)
-    parser.add_argument('--margin', type=int,
-        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
-    parser.add_argument('--random_order', 
-        help='Shuffles the order of images to enable alignment using multiple processes.', action='store_true')
-    parser.add_argument('--gpu_memory_fraction', type=float,
-        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
-    parser.add_argument('--detect_multiple_faces', type=bool,
-                        help='Detect and align multiple faces per image.', default=False)
+    parser.add_argument('--image_size', type=int, help='Image size (height, width) in pixels.', default=182)
+    parser.add_argument('--margin', type=int, help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
+    parser.add_argument('--random_order', help='Shuffles the order of images to enable alignment using multiple processes.', action='store_true')
+    parser.add_argument('--gpu_memory_fraction', type=float, help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
+    parser.add_argument('--detect_multiple_faces', type=bool, help='Detect and align multiple faces per image.', default=False)
+    parser.add_argument('--prefix', default='A_', type=str, help='Prefix of logs data. Used to place generated files on top, to find quicker')
+
     return parser.parse_args(argv)
 
 if __name__ == '__main__':
