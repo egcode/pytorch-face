0920f13539f1465fc40130fbc8146ef50bb4dca4
eg <eugene@arcules.com>
2019-08-23 11:50:06 -0700

    read dataset

--- dataset_cleanup/read_dataset.py
+++ dataset_cleanup/read_dataset.py
@@ -1,12 +1,6 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-import os
-import h5py
-import numpy as np
-
-### Requirement: name, path, embedding
-
 '''
 person1_name
     label    0
@@ -26,16 +20,61 @@ person2_name
         file_path    '/path/to/file1123123'
         embedding    [3.0, 41.1, 56.621]
 
+
+
+
+python3 dataset_cleanup/read_dataset.py
+
 '''
-# python3 dataset_cleanup/read_dataset.py
+
+import os
+import h5py
+import numpy as np
+
 ## Reading From File
+# with h5py.File('data/dataset.h5', 'r') as f:
+#     for person in f.keys():
+#         print("personName: " + str(person))
+#         print("personLabel: " + str(f[person].attrs['label']))
+
+#         for subgroup in f[person].keys() :
+#             print("\tsubgroup: " + str(subgroup))
+
+#             print("\t\tembedding data: " + str(f[person][subgroup]['embedding'][:4]))
+#             print("\t\tpath data: " + str(f[person][subgroup].attrs['file_path']))
+
+
+import scipy.cluster.hierarchy as shc
+import matplotlib.pyplot as plt
+
+# Data for each person
 with h5py.File('data/dataset.h5', 'r') as f:
     for person in f.keys():
         print("personName: " + str(person))
-        print("personLabel: " + str(f[person].attrs['label']))
+#         print("personLabel: " + str(f[person].attrs['label']))
 
-        for subgroup in f[person].keys() :
+        nrof_images = len(f[person].keys())
+        embedding_size = 512
+        embeddings_array = np.zeros((nrof_images, embedding_size))
+        label_array = np.zeros((0,0))
+        label_strings_array = np.zeros((0,0))
+
+        print("\tembedding array shape: " + str(embeddings_array.shape))
+        print("\tnumber of images: " + str(nrof_images) + "  embedding size: " + str(embedding_size))
+
+        for i, subgroup in enumerate(f[person].keys()):
+            print("\tlabel: " + str(i))
+            embeddings_array[i, :] = f[person][subgroup]['embedding'][:]
+            label_array = np.append(label_array, i)
+            
+            
             print("\tsubgroup: " + str(subgroup))
+            print("\t\tembedding data shape: " + str(f[person][subgroup]['embedding'][:].shape))
 
             print("\t\tembedding data: " + str(f[person][subgroup]['embedding'][:4]))
             print("\t\tpath data: " + str(f[person][subgroup].attrs['file_path']))
+
+        plt.figure(figsize=(10, 7))
+        plt.title(str(person))
+        dend = shc.dendrogram(shc.linkage(embeddings_array, method='ward'))
+        plt.show()
